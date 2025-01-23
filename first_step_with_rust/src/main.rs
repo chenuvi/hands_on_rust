07efa7afcbc54553244d9a55fbb631145666dced
kunchen16 <kunchen16@iflytek.com>
2025-01-23 14:23:16 +0800

    feat: storage in array

--- first_step_with_rust/src/main.rs
+++ first_step_with_rust/src/main.rs
@@ -1,12 +1,24 @@
 use std::io::stdin;
 fn main() {
+    let visitor_list = ["bert", "steve", "fred"];
+    let mut allow_in = false;
+
     println!("Hello, what's your name?");
     let name = what_is_your_name(); // (4)
-    println!("Hello, {}", name);
+    for visitor in visitor_list {
+        if visitor == &name {
+            allow_in = true;
+        }
+    }
+    if allow_in {
+        println!("Welcome to the Treehouse, {}", name);
+    } else {
+        println!("Sorry, you aren't on the list.");
+    }
 }
 
 fn what_is_your_name() -> String {
     let mut name = String::new();
     stdin().read_line(&mut name).expect("Failed to read line");
-    name
+    name.trim().to_lowercase()
 }
