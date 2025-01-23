defc71b6b10b4a266d2b4b747ac8e7e6d31e1dbe
kunchen16 <kunchen16@iflytek.com>
2025-01-23 15:01:44 +0800

    feat: vector

---
+++ REAEME.md
@@ -0,0 +1,7 @@
+# Rust 游戏开发实战 （书籍） 代码实践
+
+检查代码 
+cargo clippy
+
+代码包运行
+ cargo run -p first_step_with_rust
--- first_step_with_rust/src/main.rs
+++ first_step_with_rust/src/main.rs
@@ -1,5 +1,5 @@
 use std::io::stdin;
-
+#[derive(Debug)]
 struct Visitor {
     name: String,
     greeting: String,
@@ -14,23 +14,33 @@ impl Visitor {
     }
 
     fn greet_visitor(&self) {
-        print!("{}", self.greeting);
+        print!("{} \n", self.greeting);
     }
 }
 
 fn main() {
-    let visitor_list = [
+    let mut visitor_list = vec![
         Visitor::new("bert", "Hello Bert, enjoy your treehouse."),
         Visitor::new("steve", "Hi Steve. Your milk is in the fridge."),
         Visitor::new("fred", "Wow, who invited Fred?"),
     ];
-
-    println!("Hello, what's your name?");
-    let name = what_is_your_name(); // (4)
-    let known_visitor = visitor_list.iter().find(|visitor| visitor.name == name);
-    match known_visitor {
-        Some(visitor) => visitor.greet_visitor(),
-        None => println!("You are not on the visitor list. Please leave."),
+    loop {
+        println!("Hello, what's your name? (Leave empty and press ENTER to quit)");
+        let name = what_is_your_name(); // (4)
+        let known_visitor = visitor_list.iter().find(|visitor| visitor.name == name);
+        match known_visitor {
+            Some(visitor) => visitor.greet_visitor(),
+            None => {
+                if name.is_empty() {
+                    break;
+                } else {
+                    println!("{} is not on the visitor list.", name);
+                    visitor_list.push(Visitor::new(&name, "New friend"));
+                    println!("The final list of visitors: ");
+                    println!("{:#?} \n", visitor_list);
+                }
+            }
+        }
     }
 }
 
